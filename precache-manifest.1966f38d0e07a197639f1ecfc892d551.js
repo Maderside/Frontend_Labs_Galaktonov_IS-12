285c860982d593898aad0723d7f63d77b9d23fbe
Maderside <maderside@gmail.com>
2024-01-04 16:06:07 +0200

    Updates

---
+++ precache-manifest.1966f38d0e07a197639f1ecfc892d551.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af8a8da185dd423b186d87ad8a981919",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "ccda9a88ea552d9e2547",
+    "url": "/my-app/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "bc853196e31e2d32a6c9",
+    "url": "/my-app/static/js/2.a2b181a3.chunk.js"
+  },
+  {
+    "revision": "e2a960147ef5362ced8a",
+    "url": "/my-app/static/js/3.1b3c587a.chunk.js"
+  },
+  {
+    "revision": "ccda9a88ea552d9e2547",
+    "url": "/my-app/static/js/main.3fb7cd65.chunk.js"
+  },
+  {
+    "revision": "4d146bdfeae550b5e4f5",
+    "url": "/my-app/static/js/runtime~main.b2ff0fcd.js"
+  }
+]);
