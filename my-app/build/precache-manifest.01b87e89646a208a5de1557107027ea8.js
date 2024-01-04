996a31419efcde9f6dc278b1fa9b1fb89a526329
Maderside <maderside@gmail.com>
2024-01-04 18:41:55 +0200

    added lab7 build

---
+++ my-app/build/precache-manifest.01b87e89646a208a5de1557107027ea8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a98e5354da6a0f710e1f3fa5881311f",
+    "url": "/my-app/build/index.html"
+  },
+  {
+    "revision": "51955dc67dfcb35a76d8",
+    "url": "/my-app/build/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "bc853196e31e2d32a6c9",
+    "url": "/my-app/build/static/js/2.a2b181a3.chunk.js"
+  },
+  {
+    "revision": "e2a960147ef5362ced8a",
+    "url": "/my-app/build/static/js/3.1b3c587a.chunk.js"
+  },
+  {
+    "revision": "51955dc67dfcb35a76d8",
+    "url": "/my-app/build/static/js/main.3fb7cd65.chunk.js"
+  },
+  {
+    "revision": "fad6986ebc3731f638ae",
+    "url": "/my-app/build/static/js/runtime~main.f0be2579.js"
+  }
+]);
--- my-app/build/service-worker.js
+++ my-app/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/lab7/precache-manifest.fc1dace78c2da427d700bc80cf04859f.js"
+  "/my-app/build/precache-manifest.01b87e89646a208a5de1557107027ea8.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/lab7/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/my-app/build/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
