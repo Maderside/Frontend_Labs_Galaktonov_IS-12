87615f15591c7b51d056e1d6939c813a5bb13a28
Maderside <maderside@gmail.com>
2024-01-04 18:35:19 +0200

    added lab7 build

---
+++ my-app/build/precache-manifest.fc1dace78c2da427d700bc80cf04859f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e33d7719c6c55e044b951c110064ea6e",
+    "url": "/lab7/index.html"
+  },
+  {
+    "revision": "51955dc67dfcb35a76d8",
+    "url": "/lab7/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "bc853196e31e2d32a6c9",
+    "url": "/lab7/static/js/2.a2b181a3.chunk.js"
+  },
+  {
+    "revision": "e2a960147ef5362ced8a",
+    "url": "/lab7/static/js/3.1b3c587a.chunk.js"
+  },
+  {
+    "revision": "51955dc67dfcb35a76d8",
+    "url": "/lab7/static/js/main.3fb7cd65.chunk.js"
+  },
+  {
+    "revision": "0b74a2921553727d593f",
+    "url": "/lab7/static/js/runtime~main.79f3c64a.js"
+  }
+]);
